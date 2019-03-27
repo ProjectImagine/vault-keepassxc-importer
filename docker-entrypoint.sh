1cdb6c8f2da65d52e8ad4d7181f13b394c9550a9
Mikhail Merkulov <mihail.merkulov@gmail.com>
2019-03-27 13:46:00 +0000

    FUCK

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #set -e
 
-if [ -f ./kv.csv ] 
+if [ -f ./tmp/kv.csv ] 
 then
     echo "File exists locally"
-    if [ -s ./kv.csv ]
+    if [ -s ./tmp/kv.csv ]
     then
        echo "Skipping downloading kv file from S3"
     else
