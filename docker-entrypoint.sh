bd40d702e7aa04cb1a056da97bc9b9e95302c3ce
Mikhail Merkulov <mihail.merkulov@gmail.com>
2019-03-27 13:09:35 +0000

    FIX

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #set -e
 
-if [ -f /tmp/kv.csv ] 
+if [ -f ./kv.csv ] 
 then
-    if [ -s /tmp/kv.csv ] 
+    echo "File exists locally"
+    if [ -s ./kv.csv ]
     then
+       echo "Skipping downloading kv file from S3"
+    else
+        
         echo "Downloading kv file from S3"
         aws s3api get-object --bucket $VAULT_DATA_BUCKET --key $VAULT_DATA_FILE /tmp/kv.csv
-    else
-        echo "Skipping downloading kv file from S3"
     fi
 else
+    echo "File doesn't exist locally"
     echo "Downloading kv file from S3"
     aws s3api get-object --bucket $VAULT_DATA_BUCKET --key $VAULT_DATA_FILE /tmp/kv.csv
 fi
